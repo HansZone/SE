dfdb84a19bb1fe462ff766f4d22685ea499d764d
HansZone <wangyehan@outlook.com>
2017-04-27 17:50:47 -0400

    Add files via upload

--- www_wyh/Long_Term.php
+++ www_wyh/Long_Term.php
@@ -4,76 +4,80 @@
 -->
 
 
-
 <?php session_start();
 @$username=$_SESSION["username"];
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
 
-        <title>Group #2|Stock Forecasting System</title>
-        <meta name="description" content="" />
-        <meta name="author" content="Walking Pixels | www.walkingpixels.com" />
-        <meta name="robots" content="index, follow" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <!-- CSS styles -->
-        <link rel='stylesheet' type='text/css' href='css/wuxia-red.css' />
-        
-        <!-- Fav and touch icons -->
-        <link rel="shortcut icon" href="img/icons/favicon.ico" />
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/icons/apple-touch-icon-114-precomposed.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/icons/apple-touch-icon-72-precomposed.png" />
-        <link rel="apple-touch-icon-precomposed" href="img/icons/apple-touch-icon-57-precomposed.png" />
-        
-        <!-- JS Libs -->
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/libs/jquery.js"><\/script>')</script>
-        <script src="js/libs/modernizr.js"></script>
-        <script src="js/libs/selectivizr.js"></script>
+<!DOCTYPE html>
+<html class="no-js" lang="en"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8" />
+		<title>Group #2|Stock Forecasting System</title>
+		<meta name="description" content="" />
+		<meta name="author" content="Walking Pixels | www.walkingpixels.com" />
+		<meta name="robots" content="index, follow" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		
+		<!-- jQuery Visualize Styles -->
+		<link rel='stylesheet' type='text/css' href='css/plugins/jquery.visualize.css' />
+		
+		<!-- CSS styles -->
+		<link rel='stylesheet' type='text/css' href='css/wuxia-red.css' />
+		
+		<!-- Fav and touch icons -->
+		<link rel="shortcut icon" href="img/icons/favicon.ico" />
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/icons/apple-touch-icon-114-precomposed.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/icons/apple-touch-icon-72-precomposed.png" />
+		<link rel="apple-touch-icon-precomposed" href="img/icons/apple-touch-icon-57-precomposed.png" />
+		
+		<!-- JS Libs -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="js/libs/jquery.js"><\/script>')</script>
+		<script src="js/libs/modernizr.js"></script>
+		<script src="js/libs/selectivizr.js"></script>
 
-        <!-- Scripts -->
-        <script src="js/navbar.js"></script>
-        <script src="js/plugins/waypoints.min.js"></script>
-        <script src="js/bootstrap/bootstrap-tooltip.js"></script>
-        <script src="js/bootstrap/bootstrap-dropdown.js"></script>
-        <script src="js/bootstrap/bootstrap-collapse.js"></script>
-        
-        <script>
-            $(document).ready(function(){
-                
-                // Navbar tooltips
-                $('.navbar [title]').tooltip({
-                    placement: 'bottom'
-                });
-                
-                // Content tooltips
-                $('[role=main] [title]').tooltip({
-                    placement: 'top'
-                });
-                
-                // Dropdowns
-                $('.dropdown-toggle').dropdown();
-                
-            });
-        </script>
-</head>
-
-<body>
-
+		<script>
+			$(document).ready(function(){
+				
+				// Navbar tooltips
+				$('.navbar [title]').tooltip({
+					placement: 'bottom'
+				});
+				
+				// Content tooltips
+				$('[role=main] [title]').tooltip({
+					placement: 'top'
+				});
+				
+				// Dropdowns
+				$('.dropdown-toggle').dropdown();
+				
+				// Tabs
+				$('.demoTabs a').click(function (e) {
+					e.preventDefault();
+					$(this).tab('show');
+					$('table').trigger('visualizeRefresh');; // Refresh jQuery Visualize for hidden tabs
+				})
+				
+			});
+		</script>
+		<script type="text/javascript">
+			var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-22557155-5"]);_gaq.push(["_trackPageview"]);(function(){var b=document.createElement("script");b.type="text/javascript";b.async=true;b.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(b,a)})();
+		</script>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+	<body>
+	
 <?php
-
-// Connect to the database
-$conn = @mysql_connect("localhost","root","");
-if (!$conn){
-    die("Failed to connect database£º" . mysql_error());
-}
-$db=mysql_select_db("seteam", $conn);
-if(!$db)
-{
-  die("Failed to connect to MySQL:". mysql_error());
-}
-
+		// Connect to the database
+		$conn = @mysql_connect("localhost","root","");
+		if (!$conn){
+			die("Failed to connect database£º" . mysql_error());
+		}
+		$db=mysql_select_db("seteam", $conn);
+		if(!$db)
+		{
+			die("Failed to connect to MySQL:". mysql_error());
+		}
 
 $handle = fopen("SVM.csv","r");
 for ($i=0;$i<10;$i++) {
@@ -95,146 +99,184 @@ for ($i=0;$i<10;$i++) {
     }
 }
 fclose($handle);
-?>
+		?>
 
-<!-- Main navigation bar -->
-        <header class="navbar navbar-fixed-top">
-            <div class="navbar-inner">
-                <div class="container"> 
-                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".user">
-                        <span class="icon-user"></span>
-                    </button>
-                
-                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".navigation">
-                        <span class="icon-chevron-down"></span>
-                    </button>
-                <nav class="nav-collapse navigation">
-                        <ul class="nav" role="navigation">
-                            <li class="active"><a href="index_se.php" title="Homepage dashboard"><span class="icon-home"></span> Home</a></li>
-                        </ul>
-                    </nav>
-                    <nav class="nav-collapse user">
-                        <div class="user-info pull-right">
-                            <img src="http://placekitten.com/35/35" alt="User avatar" />
-                            <div class="btn-group">
-                                <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
-                                    <div><strong>
-                                        <?php
-                                        if($username!="") echo $username;
-                                        else echo "Visitor";?>
-                                        </strong>Balance
-                                    </div>
-                                    <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="Log_in.php"><span class="icon-signout"></span> Switch account</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </nav>
-                </div>
-            </div>
-        </header>
-<!-- /Main navigation bar -->
-
-
-
-
-<!-- Right side -->
-<div id="rightSide">
-
-    
-    <!-- Title area -->
-    <div class="titleArea" method="post">
-			<div class="button" >
-			 <form action="<?php echo $_SERVER['PHP_SELF']?>" id="validate" class="form" method="post">
-				<?php
-				$check_query = mysql_query("SELECT name from stocks where 1"); 
-				while ($row=mysql_fetch_row($check_query))
-				{
-					$sub="submit";
-					echo "<input type=".$sub." name=".$sub." value=".$row[0]." />";
-				}
-				?>				
+		<!-- Main navigation bar -->
+		<header class="navbar navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">	
+					<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".user">
+						<span class="icon-user"></span>
+					</button>
+				
+					<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".navigation">
+						<span class="icon-chevron-down"></span>
+					</button>
+				<nav class="nav-collapse navigation">
+						<ul class="nav" role="navigation">
+							<li class="active"><a href="index_se.php" title="Homepage dashboard"><span class="icon-home"></span> Home</a></li>
+						</ul>
+					</nav>
+					<nav class="nav-collapse user">
+						<div class="user-info pull-right">
+							<img src="http://placekitten.com/35/35" alt="User avatar" />
+							<div class="btn-group">
+								<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+									<div><strong>
+										<?php
+						                if($username!="") echo $username;
+                						else echo "Visitor";?>
+                						</strong>Balance
+            						</div>
+									<span class="caret"></span>
+								</a>
+								<ul class="dropdown-menu">
+									<li><a href="Log_in.php"><span class="icon-signout"></span> Switch account</a></li>
+								</ul>
+							</div>
+						</div>
+					</nav>
 				</div>
+			</div>
+		</header>
+		<!-- /Main navigation bar -->
 		
-    </div>
-    <div class="line"></div>
-    
-<?php 
+		<!-- Main content -->
+		<div class="container" role="main">
 
-	if (isset($_POST["submit"]))
-	{
-		for ($i=0;$i<10;$i++)
-		{
-			if ($result[$i][1]==$_POST["submit"])
-				$stock=$result[$i];
-		}
-		for ($i=0;$i<10;$i++)
-		{
-			if ($result2[$i][1]==$_POST["submit"])
-				$stock2=$result2[$i];
-		}
-    ?>
-    <!-- Main content wrapper -->
-    <div class="wrappers">
-    </div>
+		<!-- Secondary navigation -->
+			<div class="nav-secondary">
+				<nav>
+					<ul>
+							<li><a class="wuxify-me" href="figure.php"><span class="icon-signal"></span>Figure</a></li>
+							<li><a class="wuxify-me" href="query_SE.php"><span class="icon-picture"></span>Query</a></li>
+							<li><a class="wuxify-me" href="Prediction.php"><span class="icon-file"></span>Predict</a></li>
+							<li><a class="wuxify-me" href="news.php"><span class="icon-heart"></span>News</a></li>
+					</ul>
+				</nav>
+			</div>
+			<!-- /Secondary navigation -->
+			
+			<!-- Main data container -->
+			<!-- <div class="content">	 -->	
 
-    <?php
-}
-?>
+				<!-- Tab content -->
+				<div class="page-container tab-content">
 
-<h3>Long Term prediction for <?php echo @$_POST["submit"]?>(SVM) (200 days)</h3>
-<table align="left" cellpadding="0" cellspacing="0" width="100%" class="sTable">
-	<thead>
-		<tr>
-			<td width="20%">Ticker</td>
-			<td width="20%">Company</td>
-			<td width="20%">Last Price Date</td>
-			<td width="20%">Pattern</td>
-			<td width="20%">Predict Operation by SVM</td>
-		</tr>
-	</thead>
-	<tbody>
-		<?php 			 
-		echo "<tr>";
-		echo "<td> ". @$stock[0]." </td>";
-		echo "<td> ". @$stock[2]. "</td>";
-		echo "<td> ". @$stock[3] ."</td>";
-		echo "<td> ". @$stock[4] ."</td>";
-		echo "<td> ". @$stock[5] ."</td>";
-		echo "</tr>"
-		?>
-	</tbody>
-</table>
 
-<h3>Long Term prediction for <?php echo @$_POST["submit"]?>(RNN) (100 days)</h3>
-<table align="left" cellpadding="0" cellspacing="0" width="100%" class="sTable">
-	<thead>
-		<tr>
-			<td width="20%">Ticker</td>
-			<td width="20%">Company</td>
-			<td width="20%">Last Price Date</td>
-			<td width="20%">Tomorrow Trend</td>
-			<td width="20%">Predict Operation by RNN</td>
-		</tr>
-	</thead>
-	<tbody>
-		<?php 			 
-		echo "<tr>";
-		echo "<td> ". @$stock2[0] ."</td>";
-		echo "<td> ". @$stock2[2] ."</td>";
-		echo "<td> ". @$stock2[3] ."</td>";
-		echo "<td> ". @$stock2[4] ."</td>";
-		echo "<td> ". @$stock2[5] ."</td>";
-		echo "</tr>"
-		?>
-	</tbody>
-</table>
+					<!-- Right side -->
+					<div id="rightSide">
 
-</div>
-    </div>
-</div>
+					    
+					    <!-- Title area -->
+					    <div class="titleArea" method="post">
+								<div class="button" >
+								 <form action="<?php echo $_SERVER['PHP_SELF']?>" id="validate" class="form" method="post">
+									<?php
+									$check_query = mysql_query("SELECT name from stocks where 1"); 
+									while ($row=mysql_fetch_row($check_query))
+									{
+										$sub="submit";
+										echo "<input type=".$sub." name=".$sub." value=".$row[0]." />";
+									}
+									?>				
+								</div>
+							
+					    </div>
+					    <div class="line"></div>
+					    
+					<?php 
 
+						if (isset($_POST["submit"]))
+						{
+							for ($i=0;$i<10;$i++)
+							{
+								if ($result[$i][1]==$_POST["submit"])
+									$stock=$result[$i];
+							}
+							for ($i=0;$i<10;$i++)
+							{
+								if ($result2[$i][1]==$_POST["submit"])
+									$stock2=$result2[$i];
+							}
+					    ?>
+					    <!-- Main content wrapper -->
+					    <div class="wrappers">
+					    </div>
+
+					    <?php
+					}
+					?>
+
+					<h3>Long Term prediction for <?php echo @$_POST["submit"]?>(SVM) (200 days)</h3>
+					<table align="left" cellpadding="0" cellspacing="0" width="100%" class="sTable">
+						<thead>
+							<tr>
+								<td width="20%">Ticker</td>
+								<td width="20%">Company</td>
+								<td width="20%">Last Price Date</td>
+								<td width="20%">Pattern</td>
+								<td width="20%">Predict Operation by SVM</td>
+							</tr>
+						</thead>
+						<tbody>
+							<?php 			 
+							echo "<tr>";
+							echo "<td> ". @$stock[0]." </td>";
+							echo "<td> ". @$stock[2]. "</td>";
+							echo "<td> ". @$stock[3] ."</td>";
+							echo "<td> ". @$stock[4] ."</td>";
+							echo "<td> ". @$stock[5] ."</td>";
+							echo "</tr>"
+							?>
+						</tbody>
+					</table>
+
+					<h3>Long Term prediction for <?php echo @$_POST["submit"]?>(RNN) (100 days)</h3>
+					<table align="left" cellpadding="0" cellspacing="0" width="100%" class="sTable">
+						<thead>
+							<tr>
+								<td width="20%">Ticker</td>
+								<td width="20%">Company</td>
+								<td width="20%">Last Price Date</td>
+								<td width="20%">Tomorrow Trend</td>
+								<td width="20%">Predict Operation by RNN</td>
+							</tr>
+						</thead>
+						<tbody>
+							<?php 			 
+							echo "<tr>";
+							echo "<td> ". @$stock2[0] ."</td>";
+							echo "<td> ". @$stock2[2] ."</td>";
+							echo "<td> ". @$stock2[3] ."</td>";
+							echo "<td> ". @$stock2[4] ."</td>";
+							echo "<td> ". @$stock2[5] ."</td>";
+							echo "</tr>"
+							?>
+						</tbody>
+					</table>
+
+						</div>
+							
+				</div>
+				<!-- /Tab content -->
+			
+			</div>
+			<!-- /Main data container -->
+			
+		</div>
+		<!-- /Main content -->
+		
+		<!-- Scripts -->
+		<script src="js/navbar.js"></script>
+		<script src="js/plugins/waypoints.min.js"></script>
+		<script src="js/bootstrap/bootstrap-tooltip.js"></script>
+		<script src="js/bootstrap/bootstrap-dropdown.js"></script>
+		<script src="js/bootstrap/bootstrap-tab.js"></script>
+		<script src="js/bootstrap/bootstrap-collapse.js"></script>
+		<script src="js/plugins/visualize/jquery.visualize.min.js"></script>
+		<script src="js/plugins/visualize/jquery.visualize.tooltip.min.js"></script>
+		
+		
 </body>
 </html>
--- www_wyh/Short_Term.php
+++ www_wyh/Short_Term.php
@@ -4,78 +4,83 @@
 -->
 
 
-
 <?php session_start();
 @$username=$_SESSION["username"];
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
 
-        <title>Group #2|Stock Forecasting System</title>
-        <meta name="description" content="" />
-        <meta name="author" content="Walking Pixels | www.walkingpixels.com" />
-        <meta name="robots" content="index, follow" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <!-- CSS styles -->
-        <link rel='stylesheet' type='text/css' href='css/wuxia-red.css' />
-        
-        <!-- Fav and touch icons -->
-        <link rel="shortcut icon" href="img/icons/favicon.ico" />
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/icons/apple-touch-icon-114-precomposed.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/icons/apple-touch-icon-72-precomposed.png" />
-        <link rel="apple-touch-icon-precomposed" href="img/icons/apple-touch-icon-57-precomposed.png" />
-        
-        <!-- JS Libs -->
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/libs/jquery.js"><\/script>')</script>
-        <script src="js/libs/modernizr.js"></script>
-        <script src="js/libs/selectivizr.js"></script>
+<!DOCTYPE html>
+<html class="no-js" lang="en"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8" />
+		<title>Group #2|Stock Forecasting System</title>
+		<meta name="description" content="" />
+		<meta name="author" content="Walking Pixels | www.walkingpixels.com" />
+		<meta name="robots" content="index, follow" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		
+		<!-- jQuery Visualize Styles -->
+		<link rel='stylesheet' type='text/css' href='css/plugins/jquery.visualize.css' />
+		
+		<!-- CSS styles -->
+		<link rel='stylesheet' type='text/css' href='css/wuxia-red.css' />
+		
+		<!-- Fav and touch icons -->
+		<link rel="shortcut icon" href="img/icons/favicon.ico" />
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/icons/apple-touch-icon-114-precomposed.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/icons/apple-touch-icon-72-precomposed.png" />
+		<link rel="apple-touch-icon-precomposed" href="img/icons/apple-touch-icon-57-precomposed.png" />
+		
+		<!-- JS Libs -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="js/libs/jquery.js"><\/script>')</script>
+		<script src="js/libs/modernizr.js"></script>
+		<script src="js/libs/selectivizr.js"></script>
 
-        <!-- Scripts -->
-        <script src="js/navbar.js"></script>
-        <script src="js/plugins/waypoints.min.js"></script>
-        <script src="js/bootstrap/bootstrap-tooltip.js"></script>
-        <script src="js/bootstrap/bootstrap-dropdown.js"></script>
-        <script src="js/bootstrap/bootstrap-collapse.js"></script>
-        
-        <script>
-            $(document).ready(function(){
-                
-                // Navbar tooltips
-                $('.navbar [title]').tooltip({
-                    placement: 'bottom'
-                });
-                
-                // Content tooltips
-                $('[role=main] [title]').tooltip({
-                    placement: 'top'
-                });
-                
-                // Dropdowns
-                $('.dropdown-toggle').dropdown();
-                
-            });
-        </script>
-</head>
-
-<body>
+		<script>
+			$(document).ready(function(){
+				
+				// Navbar tooltips
+				$('.navbar [title]').tooltip({
+					placement: 'bottom'
+				});
+				
+				// Content tooltips
+				$('[role=main] [title]').tooltip({
+					placement: 'top'
+				});
+				
+				// Dropdowns
+				$('.dropdown-toggle').dropdown();
+				
+				// Tabs
+				$('.demoTabs a').click(function (e) {
+					e.preventDefault();
+					$(this).tab('show');
+					$('table').trigger('visualizeRefresh');; // Refresh jQuery Visualize for hidden tabs
+				})
+				
+			});
+		</script>
+		<script type="text/javascript">
+			var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-22557155-5"]);_gaq.push(["_trackPageview"]);(function(){var b=document.createElement("script");b.type="text/javascript";b.async=true;b.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(b,a)})();
+		</script>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+	<body>
+	
 <?php
-
-// Connect to the database
-$conn = @mysql_connect("localhost","root","");
-if (!$conn){
-    die("Failed to connect database£º" . mysql_error());
-}
-$db=mysql_select_db("seteam", $conn);
-if(!$db)
-{
-  die("Failed to connect to MySQL:". mysql_error());
-}
-
+		// Connect to the database
+		$conn = @mysql_connect("localhost","root","");
+		if (!$conn){
+			die("Failed to connect database£º" . mysql_error());
+		}
+		$db=mysql_select_db("seteam", $conn);
+		if(!$db)
+		{
+			die("Failed to connect to MySQL:". mysql_error());
+		}
 
 
-$handle = fopen("bayes(new).csv","r");
+		$handle = fopen("bayes(new).csv","r");
 for ($i=0;$i<10;$i++) {
    $data = fgetcsv($handle, 1000, ",");
 	$num = count($data);
@@ -84,53 +89,74 @@ for ($i=0;$i<10;$i++) {
     }
 }
 fclose($handle);
-?>
+		?>
 
-<!-- Main navigation bar -->
-        <header class="navbar navbar-fixed-top">
-            <div class="navbar-inner">
-                <div class="container"> 
-                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".user">
-                        <span class="icon-user"></span>
-                    </button>
-                
-                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".navigation">
-                        <span class="icon-chevron-down"></span>
-                    </button>
-                <nav class="nav-collapse navigation">
-                        <ul class="nav" role="navigation">
-                            <li class="active"><a href="index_se.php" title="Homepage dashboard"><span class="icon-home"></span> Home</a></li>
-                        </ul>
-                    </nav>
-                    <nav class="nav-collapse user">
-                        <div class="user-info pull-right">
-                            <img src="http://placekitten.com/35/35" alt="User avatar" />
-                            <div class="btn-group">
-                                <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
-                                    <div><strong>
-                                        <?php
-                                        if($username!="") echo $username;
-                                        else echo "Visitor";?>
-                                        </strong>Balance
-                                    </div>
-                                    <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="Log_in.php"><span class="icon-signout"></span> Switch account</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </nav>
-                </div>
-            </div>
-        </header>
-<!-- /Main navigation bar -->
+		<!-- Main navigation bar -->
+		<header class="navbar navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">	
+					<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".user">
+						<span class="icon-user"></span>
+					</button>
+				
+					<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".navigation">
+						<span class="icon-chevron-down"></span>
+					</button>
+				<nav class="nav-collapse navigation">
+						<ul class="nav" role="navigation">
+							<li class="active"><a href="index_se.php" title="Homepage dashboard"><span class="icon-home"></span> Home</a></li>
+						</ul>
+					</nav>
+					<nav class="nav-collapse user">
+						<div class="user-info pull-right">
+							<img src="http://placekitten.com/35/35" alt="User avatar" />
+							<div class="btn-group">
+								<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+									<div><strong>
+										<?php
+						                if($username!="") echo $username;
+                						else echo "Visitor";?>
+                						</strong>Balance
+            						</div>
+									<span class="caret"></span>
+								</a>
+								<ul class="dropdown-menu">
+									<li><a href="Log_in.php"><span class="icon-signout"></span> Switch account</a></li>
+								</ul>
+							</div>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</header>
+		<!-- /Main navigation bar -->
+		
+		<!-- Main content -->
+		<div class="container" role="main">
+
+		<!-- Secondary navigation -->
+			<div class="nav-secondary">
+				<nav>
+					<ul>
+							<li><a class="wuxify-me" href="figure.php"><span class="icon-signal"></span>Figure</a></li>
+							<li><a class="wuxify-me" href="query_SE.php"><span class="icon-picture"></span>Query</a></li>
+							<li><a class="wuxify-me" href="Prediction.php"><span class="icon-file"></span>Predict</a></li>
+							<li><a class="wuxify-me" href="news.php"><span class="icon-heart"></span>News</a></li>
+					</ul>
+				</nav>
+			</div>
+			<!-- /Secondary navigation -->
+			
+			<!-- Main data container -->
+			<div class="content">
+				
+<!--indicator-->		
+
+				<!-- Tab content -->
+				<div class="page-container tab-content">
 
 
-
-
-<!-- Right side -->
-<div id="rightSide">
+					<div id="rightSide">
 
     
     <!-- Title area -->
@@ -165,15 +191,16 @@ fclose($handle);
      <div class="wrappers">
             </div>
 			
-            <h3>Long Term prediction for <?php echo $_POST["submit"]?></h3>
+            <h1>Short Term prediction for <?php echo $_POST["submit"]?></h1>
             <br>
-			 <h6>The prediction price for the next 10 days to<?php echo $stock2[2] ?> are 
+			 <h3>The prediction price for the next 10 days to<?php echo $stock2[2] ?> are </h3>
+             
 			 <?php 
 			 echo "</br>";
 				  for ($i=3;$i<13;$i++)
 					echo $stock2[$i],"</br>";
 				  ?>
-				  </h6>
+				  
 				  <?php
 	}
 				  ?>
@@ -181,5 +208,26 @@ fclose($handle);
         </div>
     </div>
 </div>
+					
+				</div>
+				<!-- /Tab content -->
+			
+			</div>
+			<!-- /Main data container -->
+			
+		</div>
+		<!-- /Main content -->
+		
+		<!-- Scripts -->
+		<script src="js/navbar.js"></script>
+		<script src="js/plugins/waypoints.min.js"></script>
+		<script src="js/bootstrap/bootstrap-tooltip.js"></script>
+		<script src="js/bootstrap/bootstrap-dropdown.js"></script>
+		<script src="js/bootstrap/bootstrap-tab.js"></script>
+		<script src="js/bootstrap/bootstrap-collapse.js"></script>
+		<script src="js/plugins/visualize/jquery.visualize.min.js"></script>
+		<script src="js/plugins/visualize/jquery.visualize.tooltip.min.js"></script>
+		
+		
 </body>
 </html>
